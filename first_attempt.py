c21f74d4fd7f2061b5b5d8e8e110fec517c9daab
bulat <bulat@zabor.(none)>
2016-11-20 13:15:52 +0100

    arrows connected to terminal

--- first_attempt.py
+++ first_attempt.py
@@ -1,9 +1,32 @@
 print "Hello, World!"
 
-while True:
+from Tkinter import *
 
-	print "I am hungry!"
+main = Tk()
 
-end
+def leftKey(event):
+	print "Unicorn!"
+def rightKey(event):
+	print "left, no the other left!"
+def upKey(event):
+	print "Upp"
+def downKey(event):
+	print "Feasises!"
+
+
+frame = Frame(main,width=100, height=100)
+main.bind('<Left>', leftKey)
+main.bind('<Right>', rightKey)
+main.bind('<Up>', upKey)
+main.bind('<Down>',downKey)
+frame.pack()
+main.mainloop()
+
+
+#while True:
+
+#	print "I am hungry!"
+
+#end
 
 
